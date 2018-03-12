7a6010720dd5dbeb72ef01ae21edcd74f8bc947b
Andy E Phipps <a.phipps@comicrelief.com>
2018-03-12 17:33:51 +0000

    Empty form setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types'; // ES6
+//import Validation from 'react-validation';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <label htmlFor="firstname">firstname</label>
+          <input type="text" name="firstname"/><br/>
+          <label htmlFor="lastname">lastname</label>
+          <input type="text" name="lastname"/>
+        </form>
       </div>
     );
   }
